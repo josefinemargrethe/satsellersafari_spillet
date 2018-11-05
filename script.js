1734a86d55c8490f51eee3154bace2be7982e206
Josefine Margrethe Smidt Hansen <josefinemargrethe@Josefines-MacBook-Pro-2.local>
2018-11-05 19:10:31 +0100

    03.03.01 med lyd

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", sidenVises);
 
+let showSettingsEffektSound = true;
+let showSettingsMusic = true;
+
 "use strict";
 let points = 0;
 let energy = 3;
@@ -23,7 +26,6 @@ function showStart() {
     document.querySelector("#settings").classList.add("hide");
     document.querySelector("#start_foreground").classList.remove("hide");
 
-
     document.querySelector("#start").classList.remove("hide");
     document.querySelector("#play").classList.add("pulse");
 
@@ -39,7 +41,87 @@ function showSettings() {
     document.querySelector("#start_foreground").classList.add("hide");
 
     document.querySelector("#luk").addEventListener("click", showStart);
+    //document.querySelector("#luk3").addEventListener("click", startGame);
+
+
+    document.querySelector("#sfx").addEventListener("click", toggleSounds);
+    document.querySelector("#music").addEventListener("click", toggleMusic);
+
 }
+
+function toggleSounds() {
+    console.log("toggleSounds");
+
+    if (showSettingsEffektSound == true) {
+        showSettingsEffektSound = false;
+        soundsOff();
+    } else {
+        showSettingsEffektSound = true;
+        soundsOn();
+    }
+
+
+
+}
+
+function soundsOff() {
+    console.log("soundsOff");
+    document.querySelector("#sfx").classList.remove("on");
+    document.querySelector("#sfx").classList.add("off");
+
+    document.querySelector("#sfx1").muted = true;
+    document.querySelector("#sfx2").muted = true;
+    document.querySelector("#sfx3").muted = true;
+
+
+}
+
+function soundsOn() {
+    console.log("soundsOn");
+
+    document.querySelector("#sfx").classList.remove("off");
+    document.querySelector("#sfx").classList.add("on");
+
+    document.querySelector("#sfx1").muted = false;
+    document.querySelector("#sfx2").muted = false;
+    document.querySelector("#sfx3").muted = false;
+
+}
+
+
+function toggleMusic() {
+    console.log("toggleMusic");
+
+    if (showSettingsMusic == true) {
+        showSettingsMusic = false;
+        musicOff();
+    } else {
+        showSettingsMusic = true;
+        musicOn();
+    }
+
+}
+
+function musicOff() {
+    console.log("musicOff");
+
+    document.querySelector("#music").classList.remove("on");
+    document.querySelector("#music").classList.add("off");
+
+    document.querySelector("#myMusic").muted = true;
+
+}
+
+function musicOn() {
+    console.log("musicOn");
+
+    document.querySelector("#music").classList.remove("off");
+    document.querySelector("#music").classList.add("on");
+
+    document.querySelector("#myMusic").muted = false;
+
+}
+
 
 function hideStart() {
     console.log("hideStart");
@@ -49,6 +131,9 @@ function hideStart() {
     document.querySelector("#start").classList.add("hide");
     document.querySelector("#game").classList.add("show");
     document.querySelector("#startinfo").classList.remove("hide");
+
+    //  document.querySelector("#myMusic").play();
+    //    document.querySelector("#myMusic").muted = false;
 
     document.querySelector("#luk2").classList.add("show");
 
@@ -78,7 +163,6 @@ function startGame() {
 
 
     document.querySelector("#game_elements").classList.remove("hide");
-    //   document.querySelector("#game_elements").classList.add("in_out1");
 
     document.querySelector("#figur1").addEventListener("click", clickFigur);
     document.querySelector("#figur2").addEventListener("click", clickFigur);
@@ -86,10 +170,10 @@ function startGame() {
     document.querySelector("#figur4").addEventListener("click", clickFigur);
     document.querySelector("#figur5").addEventListener("click", clickFigur);
     document.querySelector("#figur6").addEventListener("click", clickFigur);
-    /*
-    document.querySelector(".type1").addEventListener("click", clickBaby);
-    document.querySelector(".type2").addEventListener("click", clickDiamant);
-    document.querySelector(".type3").addEventListener("click", clickHest);*/
+
+    document.querySelector("#myMusic").play();
+
+    //document.querySelector("#settings_knap2").addEventListener("click", showSettings);
 
     nyePositioner();
 }
@@ -120,6 +204,10 @@ function clickBaby() {
     console.log(points);
     document.querySelector("#points").innerHTML = points;
 
+    document.querySelector("#sfx1").currentTime = 0;
+    document.querySelector("#sfx1").play();
+
+
 }
 
 function clickDiamant() {
@@ -129,8 +217,10 @@ function clickDiamant() {
 
     document.querySelector("#screen").classList.add("shake");
 
+    document.querySelector("#sfx2").currentTime = 0;
+    document.querySelector("#sfx2").play();
 
-   // document.querySelector("#heart" + life).classList.add("hide");
+    // document.querySelector("#heart" + life).classList.add("hide");
 
     console.log(energy);
     console.log("select energy " + energy + "hide");
@@ -154,18 +244,16 @@ function clickHest() {
     let liv = "#liv" + energy;
     console.log(liv);
     document.querySelector(liv).classList.add("hide");
-    energy-= 3;
+    energy -= 3;
 
+    document.querySelector("#sfx3").currentTime = 0;
+    document.querySelector("#sfx3").play();
 
-    //indsæt mist 3 liv
 }
 
 function clickFigur() {
     console.log("clickFigur");
 
-    // find ud af hvilken type der er blevet klikket på
-
-    // hvis baby -> clickBaby
     if (this.classList.contains("type1")) {
         console.log("click på baby");
         clickBaby();
@@ -177,30 +265,12 @@ function clickFigur() {
         clickHest();
     }
 
-    // document.querySelector("#game_elements").classList.add("paused");
-
-    //    points++;
-    //    console.log(points);
-    //    document.querySelector("#points").innerHTML = points;
 
     console.log(this);
-    //this.classList.add("hide");
 
-    //    document.querySelector("#game_elements").classList.add("hide");
 
     nyePositioner();
 
-    //    let myNumber = Math.floor((Math.random() * 10) + 1);
-    //    console.log("tilfældigt tal " + myNumber);
-
-    //    document.querySelector("#game_elements").classList = "type1 position" + myNumber;
-
-    //this.classList = "type1 position" + myNumber;
-
-    /* let myNumberny = Math.floor((Math.random() * 3) + 1);
-     console.log("tilfældigttal " + myNumberny);
-
-     this.classList = "position1 type" + myNumberny;*/
 
     gameStatus();
 
@@ -225,6 +295,8 @@ function gameOver() {
     document.querySelector("#game_elements").classList.add("hide");
 
     document.querySelector("#gameover").classList.remove("hide");
+    document.querySelector("#gameover").classList.add("show");
+
 
     document.querySelector("#tryagain").addEventListener("click", newGame);
 }
@@ -242,7 +314,7 @@ function levelCompleted() {
 
 
 function newGame() {
-    location=location.href;
+    location = location.href;
 }
 
 /*
